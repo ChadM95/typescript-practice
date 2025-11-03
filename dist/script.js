f2e7283991f6015cb9d6c22ad38b48e0b2b28da3
Chad Malone Murphy <130190686+ChadM95@users.noreply.github.com>
2025-11-03 13:04:41 +0000

    basic dom manipulation practice

--- dist/script.js
+++ dist/script.js
@@ -1,4 +1,12 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-console.log("hello, world");
+const header = document.getElementById('header');
+const button = document.getElementById('button');
+const input = document.getElementById('input');
+button === null || button === void 0 ? void 0 : button.addEventListener('click', (e) => {
+    console.log(e);
+    if ((input === null || input === void 0 ? void 0 : input.value) && header)
+        header.textContent = 'you entered ' + input.value;
+    else if (header)
+        header.textContent = 'please enter a value';
+});
+export {};
 //# sourceMappingURL=script.js.map
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,16 @@
-console.log("hello, world");
+const header = document.getElementById('header');
+const button = document.getElementById('button');
+const input = document.getElementById('input') as HTMLInputElement | null;
+
+button?.addEventListener('click', (e) => {
+
+    console.log(e);
+
+    if(input?.value && header)
+        header.textContent = 'you entered ' + input.value;
+
+    else if (header)
+        header.textContent = 'please enter a value';
+
+
+});
