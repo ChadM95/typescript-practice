6a1d52e2943e018dd30e38d61663c7d4d7522f90
Chad Malone Murphy <130190686+ChadM95@users.noreply.github.com>
2025-11-07 16:43:01 +0000

    add logic to button click to take user input as part of url

--- dist/script.js
+++ dist/script.js
@@ -1,27 +1,47 @@
 const button = document.querySelector('#button');
-const output = document.querySelector('#output');
-const apiUrl = 'https://jsonplaceholder.typicode.com/';
-let num = 1;
+const output = document.querySelector('.output');
+const input = document.querySelector('#input');
+let inputValue;
+const apiUrl = 'https://jsonplaceholder.typicode.com/posts/';
 button === null || button === void 0 ? void 0 : button.addEventListener('click', e => {
     console.log("button clicked");
+    if (input) {
+        inputValue = Number(input.value);
+        console.log('input value = ' + inputValue);
+    }
     buttonClick();
 });
 function buttonClick() {
-    fetch(apiUrl + 'posts/' + num).then(res => {
-        if (res.ok) {
-            num++;
-            console.log("num : " + num);
-            return res.json();
-        }
-        throw new Error('bad request');
-    }).then(data => {
-        if (output)
-            output.textContent = JSON.stringify(data);
-    }).catch(error => {
-        if (output)
-            output.textContent = error;
-    });
+    if (inputValue < 1 || !inputValue) {
+        fetch(apiUrl).then(res => {
+            if (res.ok) {
+                console.log("response OK");
+                return res.json();
+            }
+            throw new Error('bad request');
+        }).then(data => {
+            if (output)
+                output.textContent = JSON.stringify(data);
+        }).catch(error => {
+            if (output)
+                output.textContent = error;
+        });
+    }
+    else if (inputValue > 0) {
+        fetch(apiUrl + inputValue).then(res => {
+            if (res.ok) {
+                console.log("response OK");
+                return res.json();
+            }
+            throw new Error('bad request');
+        }).then(data => {
+            if (output)
+                output.textContent = JSON.stringify(data);
+        }).catch(error => {
+            if (output)
+                output.textContent = error;
+        });
+    }
 }
-;
 export {};
 //# sourceMappingURL=script.js.map
--- src/script.ts
+++ src/script.ts
@@ -1,28 +1,52 @@
 const button = document.querySelector('#button');
-const output = document.querySelector('#output');
-const apiUrl = 'https://jsonplaceholder.typicode.com/'
-let num: number = 1;
+const output = document.querySelector('.output');
+const input = document.querySelector<HTMLInputElement>('#input');
+let inputValue: number;
+
+const apiUrl = 'https://jsonplaceholder.typicode.com/posts/';
 
 button?.addEventListener('click', e => {
   console.log("button clicked");
+  if (input) {
+    inputValue = Number(input.value);
+    console.log('input value = ' + inputValue);
+  }
   buttonClick();
 });
 
 function buttonClick() {
 
-  fetch(apiUrl + 'posts/' + num).then(res => {
+  if (inputValue < 1 || !inputValue) {
+    fetch(apiUrl).then(res => {
+        if(res.ok) {
+          console.log("response OK");
+          return res.json();
+        }
+        throw new Error('bad request');
+        }).then(data => {
+          if(output)
+          output.textContent = JSON.stringify(data);
+        }).catch(error => {
+          if(output)
+          output.textContent = error;
+        })
+  }
+
+  else if (inputValue > 0) {
+
+  fetch(apiUrl + inputValue).then(res => {
     if(res.ok) {
-      num++;
-      console.log("num : " + num)
+      console.log("response OK");
       return res.json();
     }
     throw new Error('bad request');
-  }).then(data => {
-    if(output)
-    output.textContent = JSON.stringify(data);
-  }).catch(error => {
-    if(output)
-    output.textContent = error;
-  })
+    }).then(data => {
+      if(output)
+      output.textContent = JSON.stringify(data);
+    }).catch(error => {
+      if(output)
+      output.textContent = error;
+    })
+  }
 
-};
+}
